e3de4e312fd2f5e4c553b0baeeda1f75faae32be
A-Hanan <a.hanan.official@gmail.com>
2024-10-27 21:47:39 +0500

    fix: resolved vue cropper import issue

--- apps/docs/.nuxt/types/middleware.d.ts
+++ apps/docs/.nuxt/types/middleware.d.ts
@@ -1,6 +1,6 @@
 import type { NavigationGuard } from 'vue-router'
 export type MiddlewareKey = string
-declare module "../../../../node_modules/.pnpm/nuxt@3.9.2_@parcel+watcher@2.4.0_@types+node@20.11.4_encoding@0.1.13_eslint@8.56.0_optionator_uaakc4xtodmo6u5zhtkbw6zi7e/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "../../../../node_modules/.pnpm/nuxt@3.9.2_@parcel+watcher@2.4.0_@types+node@20.11.4_encoding@0.1.13_eslint@8.56.0_optionator_632ichgej54ewoi5d7aj2phmji/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
   }
