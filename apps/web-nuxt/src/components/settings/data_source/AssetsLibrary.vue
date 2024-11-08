56768c0e0260e94ba767eeb357ad5774af163b9a
Yared04 <yaredt29@gmail.com>
2024-11-08 17:16:45 +0300

    fix: Modify the theme of the filemanager component

--- apps/web-nuxt/src/components/settings/data_source/AssetsLibrary.vue
+++ apps/web-nuxt/src/components/settings/data_source/AssetsLibrary.vue
@@ -26,7 +26,6 @@ const ajaxSettings = {
   url: hostUrl,
   uploadUrl: `${hostUrl}Upload`,
 }
-
 const uploadSettings = {
   autoUpload: true,
   autoClose: true,
@@ -83,7 +82,7 @@ const contextMenuSettings = {
 // Navigation pane settings
 const navigationPaneSettings = {
   visible: true,
-  maxWidth: '850px',
+  maxWidth: '1200px',
   minWidth: '140px',
 
 }
@@ -102,7 +101,11 @@ function beforeSend(args) {
     const path = json[0].path
     const fileExtension = json[3].filename.split('.').pop()
     const folder = path.split('/').slice(-2)[0]
-    if (!folderPermissions[folder].includes(fileExtension)) {
+    if (folder === 'Templates Library') {
+      args.cancel = true
+      toast.add({ severity: 'error', summary: 'Error', detail: 'You are not allowed to upload files to this folder.', life: 3000 })
+    }
+    else if (!folderPermissions[folder]?.includes(fileExtension)) {
       args.cancel = true
       // show a message if the file is not allowed
       toast.add({ severity: 'error', summary: 'Error', detail: 'The selected file type isn’t allowed in this folder.', life: 3000 })
@@ -113,4 +116,37 @@ function beforeSend(args) {
 provide('filemanager', [DetailsView, NavigationPane, Toolbar])
 </script>
 
-  <style></style>
+<!-- import stlyle from assets/css
+  -->
+  <style>
+@import url('assets/css/SyncFusion-Filemanager-custom-style.css');
+.e-input-group:not(.e-float-icon-left):not(.e-float-input)::before,
+.e-input-group:not(.e-float-icon-left):not(.e-float-input)::after,
+.e-input-group.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::before,
+.e-input-group.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::after,
+.e-input-group.e-control-wrapper:not(.e-float-icon-left):not(.e-float-input)::before,
+.e-input-group.e-control-wrapper:not(.e-float-icon-left):not(.e-float-input)::after,
+.e-input-group.e-control-wrapper.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::before,
+.e-input-group.e-control-wrapper.e-float-icon-left:not(.e-float-input) .e-input-in-wrap::after,
+.e-float-input.e-input-group:not(.e-float-icon-left) .e-float-line::before,
+.e-float-input.e-input-group:not(.e-float-icon-left) .e-float-line::after,
+.e-float-input.e-input-group.e-float-icon-left .e-input-in-wrap .e-float-line::before,
+.e-float-input.e-input-group.e-float-icon-left .e-input-in-wrap .e-float-line::after,
+.e-float-input.e-control-wrapper.e-input-group:not(.e-float-icon-left) .e-float-line::before,
+.e-float-input.e-control-wrapper.e-input-group:not(.e-float-icon-left) .e-float-line::after,
+.e-float-input.e-control-wrapper.e-input-group.e-float-icon-left .e-input-in-wrap .e-float-line::before,
+.e-float-input.e-control-wrapper.e-input-group.e-float-icon-left .e-input-in-wrap .e-float-line::after,
+.e-filled.e-input-group.e-float-icon-left:not(.e-float-input)::before,
+.e-filled.e-input-group.e-float-icon-left:not(.e-float-input)::after,
+.e-filled.e-input-group.e-control-wrapper.e-float-icon-left:not(.e-float-input)::before,
+.e-filled.e-input-group.e-control-wrapper.e-float-icon-left:not(.e-float-input)::after,
+.e-filled.e-float-input.e-input-group.e-float-icon-left .e-float-line::before,
+.e-filled.e-float-input.e-input-group.e-float-icon-left .e-float-line::after,
+.e-filled.e-float-input.e-control-wrapper.e-input-group.e-float-icon-left .e-float-line::before,
+.e-filled.e-float-input.e-control-wrapper.e-input-group.e-float-icon-left .e-float-line::after {
+  background-color: #009EE2;
+}
+.e-toolbar-items.e-tbar-pos {
+  margin-top: -20px;
+}
+</style>
