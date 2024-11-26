faf5a64c5f8a185ae3a0f07945224443c72227a9
Yared04 <yaredt29@gmail.com>
2024-11-26 07:35:19 +0300

    refactor: modify the dasboard code

---
+++ apps/web-nuxt/src/components/dashboard/KPICards.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex h-full overflow-auto p-2 space-x-4">
+    <Card title="Documents generated this month" amount="100" icon="/google-docs.png" />
+    <Card title="Card 2" amount="200" icon="https://cdn-icons-png.flaticon.com/512/2991/2991148.png" />
+    <Card title="Card 3" amount="300" icon="https://cdn-icons-png.flaticon.com/512/1822/1822899.png" />
+  </div>
+</template>
+
+<script setup>
+import Card from './Card.vue'
+</script>
